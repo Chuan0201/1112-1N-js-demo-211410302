66afe3e435afe726134037fa0a76e2159e9d733b
chuan0201 <708095241p@gmail.com>
2023-03-09 19:18:35 +0800

    W04-P1: add 3 images of my meals to menu array

---
+++ demo/w04/menu_starter_02/app_02.js
@@ -0,0 +1,61 @@
+const menu = [
+    {
+        id: 1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.59,
+        img:'./images/item-1.jpeg',
+        remote_img: ''
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, corporis.'
+    },
+    {
+        id: 2,
+        title: 'dinner double',
+        category: 'dinner',
+        price: 15.59,
+        img:'./images/item-2.jpeg',
+        remote_img: ''
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, corporis.'
+    },
+    {
+        id: 3,
+        title: 'godzilla milkshake',
+        category: 'shakes',
+        price: 15.59,
+        img:'./images/item-3.jpeg',
+        remote_img: ''
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, corporis.'
+    },
+    {
+        id: 4,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.59,
+        img:'./images/my-1.jpg',
+        remote_img: ''
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, corporis.'
+    },
+    {
+        id: 5,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.59,
+        img:'./images/item-1.jpeg',
+        remote_img: ''
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, corporis.'
+    },
+    {
+        id: 6,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.59,
+        img:'./images/item-1.jpeg',
+        remote_img: ''
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, corporis.'
+    }
+]
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('btn-container');
+
+
