37db3a2317e4217b879af3b8a7e61e91cbb1b669
Chuan0201 <708095241p@gmail.com>
2023-03-18 20:22:45 +0800

    W05-P4: call closeModal to close modal, overlay; press ESC key will call closeModal as well01

--- demo/w05-modal-starter/script_02.js
+++ demo/w05-modal-starter/script_02.js
@@ -1,24 +1,56 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log('modal', modal);
 console.log('show-modal', btnsOpenModal);
+console.log('close-modal', btnCloseModal);
 
+// const openModal = ()=>{
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+// }
+const closeModal = ()=>{
+    modal.forEach((item)=>{
+    item.classList.add('hidden');
 
-const openModal = (index) => {
-    modal.classList.remove('hidden');
+    })
+    overlay.classList.add('hidden');
+}
+
+for(let i=0; i<btnsOpenModal.length; i++ ){
+
+    btnsOpenModal[i].addEventListener('click', ()=>{
+    console.log(modal);
+    modal[i].classList.remove('hidden');
     overlay.classList.remove('hidden');
+
+    });
+    
 }
 
-for(let i=0; i<btnsOpenModal.length; i++ ) {
-    btnsOpenModal[i].addEventListener('click', openModal(i));
+for(let i=0; i<btnCloseModal.length; i++ ){
+
+    btnCloseModal[i].addEventListener('click', ()=>{
+
+    modal[i].classList.add('hidden');
+    overlay.classList.add('hidden');
+    });
+    
 }
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'ESCAPE' && !modal.classList.contains('hidden')) {
+
+
+
+overlay.addEventListener('click',closeModal);
+
+document.addEventListener('keydown',(e) =>{
+    console.log(e);
+if(e.key === 'Escape' && !overlay.classList.contains('hidden')){
         closeModal();
+
     }
-});
+})
+
