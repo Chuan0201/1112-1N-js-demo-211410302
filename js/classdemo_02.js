73626f5388aed5df2e8c36bed7670ec4dc0a0ed2
Chuan0201 <708095241p@gmail.com>
2023-04-16 15:42:28 +0800

    W09

--- js/classdemo_02.js
+++ js/classdemo_02.js
@@ -1,8 +1,8 @@
 const showClassDemo = (week) => {
-    const showDemo = document.querySelector('.demo-section')
+    const showDemo = document.querySelector('.banner-section')
   switch(week){
     case 'w1':
-      showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width= "100% height= "100%" />`;
+      showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width= "100%" height= "100%" />`;
       break;
     case 'w2':
 
